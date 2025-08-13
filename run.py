6c4b21618c78425102e3c0826dc49b093b13f987
Agustin Villarroel <azau@MacBook-Pro-de-Agustin.local>
2025-08-13 20:05:46 -0300

    a

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app, db
 from flask_migrate import Migrate
 
 app = create_app()
-migrate = Migrate(app, db)  # <- NECESARIO para habilitar flask db
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.debug = False
